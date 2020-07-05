3cdcb0739ac4136bde9cb6f304fdcddbfd8f381a
Kos-Mos <ChuyM203@gmail.com>
2020-07-05 03:50:01 -0400

    added load and save tasks function, need to reworked to work with this app

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,34 @@
-//Display Current Date at top of calender
-var calenderDate = function() {
-    var dateToday = moment().format("dddd, MMMM, Do, YYYY");
-    $("#currentDay").text(dateToday);
-    console.log('dateToday');
+//Skirpping current time for now
+
+var loadTasks = function() {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  
+    // if nothing in localStorage, create a new object to track all task status arrays
+    if (!tasks) {
+      tasks = {
+        nine: [],
+        ten: [],
+        eleven: [],
+        twelve: [],
+        one: [],
+        two: [],
+        three: [],
+        four: [],
+        five: [],
+      };
+    }
+  
+    // loop over object properties
+    $.each(tasks, function(list, arr) {
+      // then loop over sub-array
+      arr.forEach(function(task) {
+        createTask(task.text, list);
+      });
+    });
 };
+
+var saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+  loadTasks();
