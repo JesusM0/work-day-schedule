3e3d1ff5e989e9ca9fbf56b14dd2c81874704737
Kos-Mos <ChuyM203@gmail.com>
2020-07-05 23:29:49 -0400

    attempted to log tasks and save

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,51 @@
-//Skirpping current time for now
+var currentDate = moment().format("dddd, MMMM Do");
+var tasks = [];
+//Skipping current time for now
 
 var loadTasks = function() {
     tasks = JSON.parse(localStorage.getItem("tasks"));
-  
-    // if nothing in localStorage, create a new object to track all task status arrays
-    if (!tasks) {
-      tasks = {
-        nine: [],
-        ten: [],
-        eleven: [],
-        twelve: [],
-        one: [],
-        two: [],
-        three: [],
-        four: [],
-        five: [],
-      };
-    }
-  
-    // loop over object properties
-    $.each(tasks, function(list, arr) {
-      // then loop over sub-array
-      arr.forEach(function(task) {
-        createTask(task.text, list);
-      });
-    });
+    $.each(tasks, function(time, task) {
+      createTask(time, task);
+})};
+
+var createTask = function(paraTime, paraTask) {
+  //Lets create a new, dynamic containter to house tasks
+  var taskContainer = $("<div>")
+    .addClass("col-md-10")
+    .text(paraTask);
+
+  var oriContainter = $("[data-time=" + paraTime + "]");
+
+  tasks.push({time: paraTime, task: paraTask})
+  $(oriContainter).replaceWith(taskContainer);
 };
+
+$("#workslots").on("click", ".task", function() {
+  var workSummary = $(this).children(".synopsis")
+    .text()
+    .trim();
+
+  var insertText = $("<textarea>").val(workSummary);
+  $(this).children(".synopsis").replaceWith(insertText);
+  insertText.trigger("focus");
+});
+
+$("#workslots").on("click", ".saveBtn", function() {
+  var synopsis = $(this)
+  .siblings(".task")
+  .find("textarea")
+  .val();
+
+  var taskText = $("p")
+  .addClass("synopsis")
+  .text(synopsis);
+  $(this).siblings(".task").find("textarea").replaceWith(taskText);
+  saveTasks($(this)).siblings(".task");
+});
+
 
 var saveTasks = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
-  loadTasks();
+loadTasks();
